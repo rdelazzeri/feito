ce58de490f06eda90f554e8a484a024b229d40db
Rafael Delazzeri <rdelazzeri@hotmail.com>
2021-09-08 19:59:51 -0300

    ajustes no admim

--- estoque/admin.py
+++ estoque/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Estoque, EstoqueItens
 
+@admin.register(Estoque)
+class EstoqueAdmin(admin.ModelAdmin):
+    list_display = (
+        '__str__',
+        'nf',
+        'funcionario'
+    )
+    search_fields = ('nf',)
+    list_filter = ('funcionario',)
+    date_hierarchy = 'created'
 # Register your models here.
