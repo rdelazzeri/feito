cc5dbe2e563e9d5b17837202c0b87cf552f7ff6c
rdelazzeri <rdelazzeri@hotmail.com>
2021-09-09 14:47:05 -0300

    Final login/logout

--- feito/settings.py
+++ feito/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #apps de terceiros
+    'crispy_forms',
 
     #My apps
     'core',
@@ -135,3 +136,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
