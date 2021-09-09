7eea74b7f85ec162d58d2b7de955e3e69625e1c1
rdelazzeri <rdelazzeri@hotmail.com>
2021-09-09 17:57:03 -0300

    teste conexao firebird

---
+++ prod/utils/produtos_cybersul.py
@@ -0,0 +1,11 @@
+import firebirdsql
+conn = firebirdsql.connect(dsn='localhost:/cybersul/banco/dadosadm.fdb', user='sysdba', password='masterkey')
+cur = conn.cursor()
+cur.execute("select DESCRICAO from ACEC1101")
+print(cur)
+for c in cur:
+    print(c[0])
+conn.close()
+
+
+
