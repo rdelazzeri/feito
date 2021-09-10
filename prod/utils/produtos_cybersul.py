4c734569cf87ea1ca66ced3a82a0097a3f391dd6
Rafael Delazzeri <rdelazzeri@hotmail.com>
2021-09-09 23:16:47 -0300

    testes firebird

--- prod/utils/produtos_cybersul.py
+++ prod/utils/produtos_cybersul.py
@@ -1,10 +1,19 @@
 import firebirdsql
-conn = firebirdsql.connect(dsn='localhost:/cybersul/banco/dadosadm.fdb', user='sysdba', password='masterkey')
+conn = firebirdsql.connect(
+    dsn='localhost:/cybersul/banco/dadosadm.fdb', 
+    user='sysdba', 
+    password='masterkey',
+    dialect='1'
+    )
 cur = conn.cursor()
-cur.execute("select DESCRICAO from ACEC1101")
+cur.execute("select CODIGO_CLIENTE from AUGC0301")
 print(cur)
-for c in cur:
-    print(c[0])
+n=0
+for c in cur.fetchall():
+    n= n+1
+    #print(c + ' - ' + str(n))
+    print (n) 
+
 conn.close()
 
 
